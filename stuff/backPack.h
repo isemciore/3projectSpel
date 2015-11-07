243885f22117a8d61ce97e29154986d11d2709f2
isemciore <erik.zhang92@gmail.com>
2015-11-07 16:45:18 +0100

    --

---
+++ stuff/backPack.h
@@ -0,0 +1,16 @@
+//
+// Created by erik on 2015-11-07.
+//
+
+#ifndef INC_3PROJECTSPEL_BACKPACK_H
+#define INC_3PROJECTSPEL_BACKPACK_H
+
+namespace wumpus_game {
+
+    class backPack {
+
+    };
+
+}
+
+#endif //INC_3PROJECTSPEL_BACKPACK_H
